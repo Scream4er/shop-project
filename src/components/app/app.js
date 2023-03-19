2769503bdc97ef7678cf236dc2caaf9364294b45
ScreamKO <Chaykov.Artem1@gmail.com>
2023-03-19 21:58:15 +0100

    New shop project
    Use Functinal components

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+import Categories from "./components/Category/Categories";
+
+
+const App = () => {
+    return (
+        <div className="categories-container">
+
+            <Categories />
+        </div>
+    );
+}
+
+
+export default App;
---
+++ src/components/categoryItem/categoryItem.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./categoryItem.styles.scss";
+
+const CategoryItem = ({category}) => {
+    const {imageUrl, title} = category;
+
+    return (
+        <div className="category-container">
+                <div className="background-image" style={{
+                    backgroundImage: `url(${imageUrl})`,
+                }}
+                />
+                <div className="category-body-container">
+                    <h1>{title}</h1>
+                    <p>Shop now</p>
+                </div>
+        </div>
+    )
+}
+
+export default CategoryItem;
