32a2842d5abd69fbfb6857cdbeac65a6cea9d216
ScreamKO <Chaykov.Artem1@gmail.com>
2023-03-26 15:23:55 +0200

    Add sign in as well as furebase utils

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,36 +1,19 @@
 import "./app.styles.scss";
-import Directory from "../directory";
+import {Routes, Route} from "react-router-dom";
+import Home from "../../routes/home";
+import Navigation from "../../routes/navigation";
+import SignIn from "../../routes/sign-in";
 
 const App = () => {
-    const categories = [
-        {
-            id: 1,
-            title: 'hats',
-            imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-        },
-        {
-            id: 2,
-            title: 'jackets',
-            imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-        },
-        {
-            id: 3,
-            title: 'sneakers',
-            imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-        },
-        {
-            id: 4,
-            title: 'womens',
-            imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-        },
-        {
-            id: 5,
-            title: 'mens',
-            imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-        },
-    ];
+   return (
+       <Routes>
+           <Route path='/' element={<Navigation />}>
+               <Route path='sign-in' element={<SignIn />}/>
+               <Route index element={<Home />}/>
+           </Route>
 
-    return <Directory categories={categories} />;
+       </Routes>
+   )
 };
 
 export default App;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,58 @@
+import { initializeApp } from 'firebase/app';
+import {
+    getAuth,
+    signInWithPopup,
+    signInWithRedirect,
+    GoogleAuthProvider
+} from 'firebase/auth';
+import {
+    getFirestore,
+    doc,
+    getDoc,
+    setDoc
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCu30sff5Vyfq6W0nSpJDkALHx4Ijt0OKo",
+    authDomain: "shop-project-76c59.firebaseapp.com",
+    projectId: "shop-project-76c59",
+    storageBucket: "shop-project-76c59.appspot.com",
+    messagingSenderId: "373772740605",
+    appId: "1:373772740605:web:712fdd8a04965c82dd5b94"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, "users", userAuth.uid);
+
+    const userSnapshot = await getDoc(userDocRef);
+
+    if (!userSnapshot.exists()) {
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            });
+        } catch (error) {
+            console.log('Error creating user', error.message);
+        }
+    }
+
+    return userDocRef;
+}
